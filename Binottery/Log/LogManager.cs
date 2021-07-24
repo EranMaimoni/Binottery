2c1da9be34aec135b5a9a108e9a825668e250d36
Eran Maimoni <eranmaimoni.x@gmail.com>
2021-07-24 18:52:56 +0300

    add log

---
+++ Binottery/Log/LogManager.cs
@@ -0,0 +1,13 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Binottery.Log
+{
+    public static class LogManager
+    {
+        public static readonly ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    }
+}
